4d994e3c02f0907660f8800fead93f021b2c07bd
Alfy S S <alfyss56@gmail.com>
2025-01-25 23:21:07 +0530

    added search button

--- skillmatrix/lib/explorepage.dart
+++ skillmatrix/lib/explorepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:skillmatrix/square.dart';
 
 class Explorepage extends StatelessWidget {
   const Explorepage({super.key});
@@ -8,21 +7,41 @@ class Explorepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: ListView(
-        children: [
-          MySquare(),
-          MySquare(),
-          MySquare(),
-          MySquare(),
-          MySquare(),
-      
-      
-        ],
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5, // specify the number of items
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 100,
+                    margin: EdgeInsets.all(8.0),
+                    color: Colors.blue,
+                    child: Center(
+                      child: Text(
+                        'Item $index',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
-    
-
-    
   }
 }
